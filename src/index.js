996fbff43eaf9057aae1f30ae36f20e9a3700a1f
Mark C Mitchell <mark@markcmitchell.net>
2016-12-07 12:54:52 +1100

    add boilerplate threejs code to draw a cube

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import * as THREE from 'three'
+
+const scene = new THREE.Scene()
+const renderer = new THREE.WebGLRenderer()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+camera.position.z = 3
+
+let cube = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshNormalMaterial()
+)
+
+scene.add(cube)
+
+let render = () => {
+  requestAnimationFrame(render)
+  cube.rotation.y += 0.004
+  renderer.render(scene, camera)
+}
+
+render()
