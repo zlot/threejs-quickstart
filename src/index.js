b38c9d53aa00af9396d31d6ff465aaeca4e70ec2
Mark C Mitchell <mark@markcmitchell.net>
2016-12-07 15:55:54 +1100

    utilise module orbit-controls

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import * as THREE from 'three'
+import OrbitControls from './controls/orbit-controls'
 
 const scene = new THREE.Scene()
 const renderer = new THREE.WebGLRenderer()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const controls = new OrbitControls(camera)
 
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
