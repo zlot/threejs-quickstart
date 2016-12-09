6c9719b19386f0e1c19c0197474d68b319c86315
Mark C Mitchell <mark@markcmitchell.net>
2016-12-09 16:06:54 +1100

    minor whitespace refactor

--- src/setup.js
+++ src/setup.js
@@ -4,7 +4,7 @@ import stats from './helpers/displayStats'
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
 camera.position.set(0, 0, 40)
 camera.lookAt(scene.position)
 
