b523a0199e999ea777747d6a777c650023a4e059
Mark C Mitchell <mark@markcmitchell.net>
2016-12-14 16:31:55 +1100

    simplify example2

--- src/examples/cubeCameraExample2.js
+++ src/examples/cubeCameraExample2.js
@@ -1,66 +1,59 @@
 import * as THREE from 'three'
-import { scene, gui, renderer, renderLoop } from '../setup'
-import ThreePointLighting from '../helpers/ThreePointLighting'
-
-let lightColors = {
-  keyLightColor: '#5a00ff',
-  fillLightColor: '#fced00',
-  ambientLightColor: '#080908'
-}
-let lights = new ThreePointLighting(lightColors)
-lights.createHelpers()
+import { scene, camera, renderer, renderLoop } from '../setup'
 
 /*** Skybox ***/
 let skybox = new THREE.CubeTextureLoader()
   .setPath('../../textures/swedish_royal_castle/')
   .load(['px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg'])
 skybox.mapping = THREE.CubeReflectionMapping
-window.s = skybox
 scene.background = skybox
 
-/*** cube camera ***/
-let cubeCamera = new THREE.CubeCamera(1, 1000, 1024)
-let cubeCamera2 = new THREE.CubeCamera(1, 1000, 1024)
-
-let cube = new THREE.Mesh(new THREE.BoxGeometry(5, 50, 20), new THREE.MeshBasicMaterial({
-  envMap: cubeCamera.renderTarget.texture
-}))
-scene.add(cube)
-cube.add(cubeCamera)
+/** Camera **/
+camera.position.set(150, 35, 0)
 
 
-let cube2 = new THREE.Mesh(new THREE.BoxGeometry(5, 50,50), new THREE.MeshBasicMaterial({
-  envMap: cubeCamera2.renderTarget.texture
-}))
-cube2.rotation.y = Math.PI/2
-cube2.position.set(30, 0, 30)
-cubeCamera2.rotation.y = -Math.PI/2
+let mirrors = []
 
-scene.add(cube2)
-cube2.add(cubeCamera2)
+let createMirrorBox = (boxGeometry) => {
+  let cubeCamera = new THREE.CubeCamera(1, 1000, 1024)
+  let mirror = new THREE.Mesh(boxGeometry, new THREE.MeshBasicMaterial({
+    envMap: cubeCamera.renderTarget.texture
+  }))
+  mirror.userData.cubeCamera = cubeCamera
+  scene.add(mirror)
+  mirror.add(cubeCamera) // note we add the cubeCamera to the mirror, so it follows position
+  mirrors.push(mirror)
+  return mirror
+}
 
-let normalCube = new THREE.Mesh(new THREE.BoxGeometry(25, 25, 25), new THREE.MeshNormalMaterial())
-normalCube.position.x = 55
+let mirror1 = createMirrorBox(new THREE.BoxGeometry(5, 100, 50))
+mirror1.position.set(-20, 0, 0)
+let mirror2 = createMirrorBox(new THREE.BoxGeometry(5, 100, 20))
+
+// NOTE the rotation of mirror2, and the OPPOSITE rotation of the cubeCamera!
+// Otherwise, reflection will be wrong!
+mirror2.rotation.y = Math.PI/2
+mirror2.userData.cubeCamera.rotation.y = -Math.PI/2
+mirror2.position.set(30, 0, 30)
+
+
+let normalCube = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 30), new THREE.MeshNormalMaterial())
+normalCube.position.x = 30
 scene.add(normalCube)
 
-window.r = cubeCamera.rotation
 renderLoop(() => {
   let time = performance.now()*0.001
 
-  cube.position.z = 10*Math.sin(time/2)
   normalCube.rotation.y = time
+  normalCube.rotation.x = Math.sin(time) * 2
+  normalCube.position.z = Math.sin(time) * 25 - 30
 
-  normalCube.position.z = Math.sin(time) * 25 - 60
+  mirror1.position.z = 30*Math.sin(time*1.5) - 15
 
-
-  // hide cube, otherwise the cubemap texture can't see through it!
-  cube.visible = false
-	cubeCamera.updateCubeMap(renderer, scene)
-  cube.visible = true
-
-  cube2.visible = false
-	cubeCamera2.updateCubeMap(renderer, scene)
-  cube2.visible = true
-
-
+  // hide cube, otherwise the cubeCamera can't see through it!
+  mirrors.forEach(mirror => {
+    mirror.visible = false
+    mirror.userData.cubeCamera.updateCubeMap(renderer, scene)
+    mirror.visible = true
+  })
 })
