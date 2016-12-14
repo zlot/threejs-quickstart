df68151926d7f72812d25b68b10ada18917743fd
Mark C Mitchell <mark@markcmitchell.net>
2016-12-14 12:35:15 +1100

    add cubeCameraExample

---
+++ src/examples/cubeCameraExample.js
@@ -0,0 +1,70 @@
+import * as THREE from 'three'
+import { scene, gui, renderer, renderLoop } from '../setup'
+import ThreePointLighting from '../helpers/ThreePointLighting'
+
+renderer.gammaInput = true;
+      renderer.gammaOutput = true;
+
+let lightColors = {
+  keyLightColor: '#5a00ff',
+  fillLightColor: '#fced00',
+  ambientLightColor: '#080908'
+}
+let lights = new ThreePointLighting(lightColors)
+lights.createHelpers()
+
+/*** Skybox ***/
+let skybox = new THREE.CubeTextureLoader()
+  .setPath('../../textures/skybox/')
+  .load(['px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg'])
+skybox.mapping = THREE.CubeReflectionMapping // THREE.CubeReflectionMapping
+scene.background = skybox
+
+/*** cube camera ***/
+let cubeCamera = new THREE.CubeCamera(1, 1000, 128)
+scene.add(cubeCamera)
+
+let cubes = []
+
+let createCube = () => {
+  let cube = new THREE.Mesh(new THREE.BoxGeometry(5, 5, 5), new THREE.MeshNormalMaterial())
+  cubes.push(cube)
+  scene.add(cube)
+}
+createCube()
+createCube()
+createCube()
+
+let createSphere = () => {
+  let geo = new THREE.SphereGeometry(6, 64, 32)
+  let mat = new THREE.MeshStandardMaterial({
+    roughness: 0.2,
+    metalness: 1,
+    envMap: cubeCamera.renderTarget
+  })
+  return new THREE.Mesh(geo, mat)
+}
+
+let sphere = createSphere()
+scene.add(sphere)
+sphere.visible = true
+
+renderLoop(() => {
+  let time = performance.now()*0.001
+
+  for(let i=0; i<cubes.length; i++) {
+    let offset = (i+1)/cubes.length * Math.PI*2
+    cubes[i].position.x = 15*Math.cos(time + offset)
+    cubes[i].position.z = 15*Math.sin(time + offset)
+  }
+
+  // hide sphere, otherwise the cubemap texture can't see through it!
+  sphere.visible = false
+	cubeCamera.updateCubeMap(renderer, scene)
+  sphere.visible = true
+
+  // lights.keyLight.position.x = 10*Math.cos(time)
+  // lights.keyLight.position.z = 10*Math.sin(time)
+  // lights.fillLight.position.y = 10*Math.cos(time)
+  // lights.fillLight.position.z = 10*Math.sin(time)
+})
