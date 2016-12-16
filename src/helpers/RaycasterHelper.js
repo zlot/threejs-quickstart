bc64ac1a83a8dfb4374e2a47bd3380cea84974ec
Mark C Mitchell <mark@markcmitchell.net>
2016-12-16 18:30:14 +1100

    add RaycasterHelper.js

---
+++ src/helpers/RaycasterHelper.js
@@ -0,0 +1,42 @@
+import { Raycaster, EventDispatcher } from 'three'
+import { scene, camera, mouse } from '../setup'
+
+
+export default class RaycasterHelper {
+
+  constructor(objectsToCheck = scene.children) {
+    this.raycaster = new Raycaster()
+    this.objectsToCheck = objectsToCheck
+
+    this.selected = null
+    this.noFurtherIntersects = true
+  }
+
+  checkForIntersects() {
+    this.raycaster.setFromCamera(mouse, camera)
+    let intersects = this.raycaster.intersectObjects(this.objectsToCheck)
+
+    if(intersects.length > 0) {
+      // if we're intersecting a new object
+      if(this.selected != intersects[0].object) {
+        this.dispatchEvent({
+          type: 'newIntersection',
+          object: intersects[0].object,
+          previousObject: this.selected
+        })
+        this.selected = intersects[0].object
+      }
+    } else {
+      if(this.selected) {
+        this.dispatchEvent({
+          type: 'zeroIntersects',
+          previousObject: this.selected
+        })
+      }
+      this.selected = null
+    }
+  }
+}
+
+// Give RaycasterHelper EventDispatcher abilities
+Object.assign(RaycasterHelper.prototype, EventDispatcher.prototype)
