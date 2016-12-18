0e3a86ac2b9500e59ea238313f275e7d754a4974
Mark C Mitchell <mark@markcmitchell.net>
2016-12-18 11:03:07 +1100

    add gui for anisotropy and bumpScale tweaking

--- src/examples/texture-mapping-bump.js
+++ src/examples/texture-mapping-bump.js
@@ -1,17 +1,21 @@
 import * as THREE from 'three'
-import { scene, gui, renderLoop } from '../setup'
+import { scene, camera, gui, renderer, renderLoop } from '../setup'
 import ThreePointLighting from '../helpers/ThreePointLighting'
 
 new ThreePointLighting({
   ambientLightColor: 0x999999
 })
 
+camera.position.set(0, 0, 25)
+
 let texLoader = new THREE.TextureLoader()
+let rocksTexture = texLoader.load('../textures/rocks.jpg')
 
 let matWithBump = new THREE.MeshPhongMaterial({
-  map: texLoader.load('../textures/rocks.jpg'),
-  bumpMap: texLoader.load('../textures/rocks.jpg'),
-  bumpScale: 0.6,
+  map: rocksTexture,
+  bumpMap: rocksTexture,
+  bumpScale: 0.3,
+  anisotropy: 16
 })
 
 let matWithoutBump = new THREE.MeshPhongMaterial({
@@ -26,7 +30,12 @@ let meshWithoutBump = new THREE.Mesh(new THREE.BoxBufferGeometry(150,10,150), ma
 meshWithoutBump.position.y = 15
 scene.add(meshWithoutBump)
 
+/*** GUI ***/
+gui.add(matWithBump, 'bumpScale').min(0).max(0.8)
+gui.add(rocksTexture, 'anisotropy').min(1).max(renderer.getMaxAnisotropy()).step(1).onChange(function() {
+  rocksTexture.needsUpdate = true
+})
 
 renderLoop(() => {
-  // mesh.rotation.y += 0.001
+  meshWithBump.rotation.y += 0.001
 })
