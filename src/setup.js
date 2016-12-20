48d3385985266a42ef2272de789b32b848fc0002
Mark C Mitchell <mark@markcmitchell.net>
2016-12-20 12:13:10 +1100

    setup.js now exports an init() function. This must be called in main js entry. From here we can set options. This commit includes a physics boolean, for Physijs 2.0 integration

--- src/setup.js
+++ src/setup.js
@@ -3,68 +3,102 @@ import TrackballControls from './controls/TrackballControls'
 import Stats from 'stats.js'
 import dat from 'dat-gui'
 
-const scene = new THREE.Scene()
+let scene, camera, controls, mouse, renderer, renderLoop, gui
+let Physijs
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
-camera.position.set(0, 0, 40)
-camera.lookAt(scene.position)
+function init({
+  physics = false
+} = {}) {
 
-let mouse = new THREE.Vector2()
+  if(physics) {
+    Physijs = require('physijs/dist/physi.js')
+    scene = new Physijs.Scene(__dirname + '/examples/physijs/physijs-worker.js', {
+      gravity: {x: 0, y: -9.8, z: 0}
+    })
+  } else {
+    scene = new THREE.Scene()
+  }
 
-let gui = new dat.gui.GUI()
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
+  camera.position.set(0, 0, 40)
+  camera.lookAt(scene.position)
 
-scene.add(new THREE.AxisHelper(20))
+  mouse = new THREE.Vector2()
 
-let stats = new Stats()
-document.body.appendChild(stats.dom)
+  gui = new dat.gui.GUI()
 
-let renderer = createRenderer(true)
+  scene.add(new THREE.AxisHelper(20))
 
-function createRenderer(shadowsEnabled = true) {
-  let r = new THREE.WebGLRenderer()
-  r.setSize(window.innerWidth, window.innerHeight)
-  r.setPixelRatio(window.devicePixelRatio)
-  if(shadowsEnabled)
-    r.shadowMap.enabled = true
-  document.body.appendChild(r.domElement)
-  return r
+  const stats = new Stats()
+  document.body.appendChild(stats.dom)
+
+  renderer = createRenderer(true)
+
+  function createRenderer(shadowsEnabled = true) {
+    let r = new THREE.WebGLRenderer()
+    r.setSize(window.innerWidth, window.innerHeight)
+    r.setPixelRatio(window.devicePixelRatio)
+    if(shadowsEnabled)
+      r.shadowMap.enabled = true
+    document.body.appendChild(r.domElement)
+    return r
+  }
+
+  renderLoop = customAnimation => {
+    let animate = customAnimation
+    let physics_framerate = 1000/60
+    
+    if(physics) {
+      function stepPhysics() {
+        scene.step(physics_framerate/1000, undefined, render)
+      }
+      function render() {
+        requestAnimationFrame(stepPhysics)
+        animate()
+        controls.update()
+        stats.update()
+        renderer.render(scene, camera)
+      }
+      stepPhysics()
+
+    } else {
+      (function render() {
+        requestAnimationFrame(render)
+        animate()
+        controls.update()
+        stats.update()
+        renderer.render(scene, camera)
+      })()
+    }
+  }
+
+  controls = new TrackballControls(camera, renderer.domElement) // s to zoom, d to pan
+  controls.maxDistance = 200;
+
+  window.addEventListener('resize', function onWindowResized(e) {
+    renderer.setSize(window.innerWidth, window.innerHeight)
+  	camera.aspect = window.innerWidth/window.innerHeight
+  	camera.updateProjectionMatrix()
+  })
+
+  window.addEventListener('mousemove', function onMouseMove(e) {
+    // calculate mouse position in normalized device coordinates
+    // (-1 to +1) for both components
+    mouse.x = (e.clientX/window.innerWidth)*2 - 1
+    mouse.y = -(e.clientY/window.innerHeight)*2 + 1
+  })
 }
 
-function renderLoop(customAnimation) {
-  let animate = customAnimation;
 
-  (function render() {
-    requestAnimationFrame(render)
-    animate()
-
-    controls.update()
-    stats.update()
-    renderer.render(scene, camera)
-  })()
-}
-
-const controls = new TrackballControls(camera, renderer.domElement) // s to zoom, d to pan
-controls.maxDistance = 200;
-
-window.addEventListener('resize', function onWindowResized(e) {
-  renderer.setSize(window.innerWidth, window.innerHeight)
-	camera.aspect = window.innerWidth/window.innerHeight
-	camera.updateProjectionMatrix()
-})
-
-window.addEventListener('mousemove', function onMouseMove(e) {
-  // calculate mouse position in normalized device coordinates
-  // (-1 to +1) for both components
-  mouse.x = (e.clientX/window.innerWidth)*2 - 1
-  mouse.y = -(e.clientY/window.innerHeight)*2 + 1
-})
 
 export {
+  init,
   scene,
   camera,
   controls,
   mouse,
   renderer,
   renderLoop,
-  gui
+  gui,
+  Physijs
 }
