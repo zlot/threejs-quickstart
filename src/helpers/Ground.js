da00df05ff8b73199a35205627815dcd7f6ef16f
Mark C Mitchell <mark@markcmitchell.net>
2016-12-21 17:34:32 +1100

    add option/default color to Ground helper

--- src/helpers/Ground.js
+++ src/helpers/Ground.js
@@ -4,13 +4,16 @@ import { scene } from '../setup'
 export default class Ground {
 
   constructor({
+    color = 0x888888,
     y = -20
   } = {}) {
 
     let geo = new THREE.PlaneGeometry(100, 100)
     geo.rotateX(-Math.PI/2)
 
-    let mat = new MeshPhongMaterial()
+    let mat = new MeshPhongMaterial({
+      color
+    })
 
     this.mesh = new THREE.Mesh(geo, mat)
     this.mesh.position.set(0, y, 0)
