d090ead848dc22b55ac8c6bb85c8520475e2d085
Mark C Mitchell <mark@markcmitchell.net>
2016-12-21 17:34:14 +1100

    give helper Ground ability to take in options to constructor. Default y = -20

--- src/helpers/Ground.js
+++ src/helpers/Ground.js
@@ -3,15 +3,19 @@ import { scene } from '../setup'
 
 export default class Ground {
 
-  constructor() {
+  constructor({
+    y = -20
+  } = {}) {
+
     let geo = new THREE.PlaneGeometry(100, 100)
     geo.rotateX(-Math.PI/2)
 
     let mat = new MeshPhongMaterial()
 
     this.mesh = new THREE.Mesh(geo, mat)
+    this.mesh.position.set(0, y, 0)
 
-    this.mesh.position.set(0, -20, 0)
+
     this.mesh.receiveShadow = true
     scene.add(this.mesh)
   }
